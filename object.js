4a2f0e89bd4abe358879ba48538eee98dc150869
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-11 09:13:38 +0600

    added some object

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-let list = {
+/* let list = {
     name: "Opi",
     age: 34,
 }
@@ -7,3 +7,21 @@ list.education = "BBA";
 const obj = Object();
 obj.session = 2021,
 console.log(obj)
+ */
+
+const point = {
+    x: 90,
+    y:23,
+    z:10,
+}
+point.x = 1
+point['y'] = 24
+const update = 'z'
+point[update] = 100
+delete point[update]
+console.log(point)
+
+// console.log(point.x + point.z)
+// console.log(point['z'])
+// const search = 'y';
+// console.log(point[search])
