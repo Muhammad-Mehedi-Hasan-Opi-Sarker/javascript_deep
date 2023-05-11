fd659c5a24bf2c62179c6e8df65cfcd6995776e8
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-11 15:58:01 +0600

    first class function

---
+++ function/firstClass.js
@@ -0,0 +1,21 @@
+const add = function (a,b){
+    return a+b;
+}
+const sum = add;
+const result = sum(3,3)
+// console.log(result)
+
+let arr = [];
+arr.push(add);
+console.log(arr[0](2,2))
+
+
+/* 
+
+rule: 
+1. A Function can be stored in a variable
+2. A Function can be stored in an array
+3. A Function can be stored in an object
+4. We can create function as need
+
+*/
