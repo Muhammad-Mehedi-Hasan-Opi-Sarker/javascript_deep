24f2c1d733edb570d8d2e9a60158cecfd4ad94aa
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-11 17:16:54 +0600

    added call back function

---
+++ function/callback.js
@@ -0,0 +1,27 @@
+function simple(a, b, cb) {
+  const c = a + b; //15
+  const d = a - b; //5
+  const result = cb(c, d); // 15, 5
+  return result; // 20
+}
+
+function sum(a, b) {
+  return a + b;
+}
+const resultSum = simple(10, 5, sum);
+console.log(resultSum);
+// resultSum function and sub both same;
+const sub = simple(10, 5, function substraction(a, b) {
+  return a - b;
+});
+console.log("Subtraction:", sub);
+
+const multi = simple(5,8, function (a, b) {
+  return a * b;
+});
+console.log(multi);
+
+const division = simple(10, 5, function div(a, b) {
+  return a / b;
+});
+console.log(division);
---
+++ function/closer.js
@@ -0,0 +1,8 @@
+var b = 10;
+function a() {
+  console.log()
+  
+}
+
+var abc = a();
+console.log(abc);
