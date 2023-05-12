fe7d7227321ebd625559e4d4203a0499f51fb518
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-12 20:29:58 +0600

    coment add

--- function/reduce.js
+++ function/reduce.js
@@ -1,10 +1,20 @@
+/* 
+
+here have two reducer function. one function made by javascript and
+other function made by me.
+this two function is same purpose.
+
+*/
 const arr = [2, 3, 4, 5, 67, 8];
 
+// one way 
 const add = arr.reduce((acc, curr) => {
   return acc + curr;
 });
 // console.log(add);
 
+
+// other way without method 
 function reduce(arr, cb, acc) {
   for (let i = 0; i < arr.length; i++) {
     acc = cb(acc, arr[i]);
